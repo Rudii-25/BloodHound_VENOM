c58cd33450cc9d787fa5d5a33800b12ee3af9bad
Rudra-hacker-25 <120879978+Rudra-hacker-25@users.noreply.github.com>
2026-01-21 21:22:36 +0530

    Fix 404 return home for HashRouter

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Assembly from "./pages/Assembly";
 import Firmware from "./pages/Firmware";
@@ -20,7 +20,6 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/assembly" element={<Assembly />} />
@@ -32,7 +31,6 @@ const App = () => (
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
